816c9ccc755ceff32f047b8b9cc4a3b58765d493
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-04-14 19:20:05 +0530

    error init

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,15 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
-
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 
 	//:= operator is a shortcut for declaring and initializing a variable in one line.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name) //%v format verb
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "rsc.io/quote"
-    "example.com/greetings"
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+	"rsc.io/quote"
 )
 
 func main() {
-    fmt.Println(quote.Go())
-    message := greetings.Hello("Gladys")
-    fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	fmt.Println(quote.Go())
+	message, err := greetings.Hello("Kevin")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
 }
